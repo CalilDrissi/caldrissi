5f0ba9f97cda768f1646641a3c652c8b058ca936
Calil Drissi <calil.drissi@gmail.com>
2022-05-09 22:04:04 +0100

    skeleton in place

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      margin: 0;
+      padding: 0;
+    }
+
+  body {
+    background: #3434;
+    font-family: sans-serif ;
+  }
+`
+
+
+const Container = styled.div`
+      display: flex;
+      justify-content: center ;
+      width: 100%;
+      height:100%;
+      gap: 1rem;
+`
+
+const SectionOne = styled.section`
+      width: 40vw;
+      min-height: 80vh;
+      background: #5454;
+      padding: 1rem;
+`
+
+const SectionTwo = styled.section`
+    width: 40vw;
+    min-height: 80vh;
+    background: #5555 ;
+    padding: 1rem;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle />
+    <Container>
+        <SectionOne> 
+          <h1> Who I am</h1>
+          </SectionOne>
+        <SectionTwo>
+          <h1>My Works</h1>
+        </SectionTwo>
+    </Container>
+    </>
   );
 }
 
