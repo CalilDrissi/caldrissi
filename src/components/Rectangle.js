05f721a3482536d9ffe1cfd4853841801cdfda9b
Calil Drissi <calil.drissi@gmail.com>
2022-05-10 20:35:42 +0000

    cards structure added

---
+++ src/components/Rectangle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Rectangle = () => {
+  return (
+    <div>
+        <h2>Title</h2>
+        <p>Description</p>
+        <div>Tech Stack</div>
+        <div>Links</div>
+    </div>
+  )
+}
+
+export default Rectangle
