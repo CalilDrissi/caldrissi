e7ec5961cb03bcc7aabee5db78d2ee4608098e67
Calil Drissi <calil.drissi@gmail.com>
2022-05-10 20:37:01 +0000

    post link structure added

---
+++ src/components/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+        <h2>Post title</h2>
+        <p>Description</p>
+        <div>tags</div>
+    </div>
+  )
+}
+
+export default Post
